6abe7f263c30189a6f362aa084403e0c1683f689
K.K. Lamberty <lamberty@morris.umn.edu>
2025-01-20 02:19:06 -0600

    Remove everything but `server` folder

--- client/cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'cypress';
-
-export default defineConfig({
-  e2e: {
-    setupNodeEvents: (on, config) => {
-      require('./cypress/plugins/index.ts').default(on, config);
-    },
-  },
-});
